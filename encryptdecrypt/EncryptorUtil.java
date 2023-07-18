c7dab0082666f23c602ca18a67eaf2c1ac7c0894
Sirio <sirioroberto1@hotmail.com>
2023-07-17 21:26:18 -0300

    Decoupled algorithms

--- encryptdecrypt/EncryptorUtil.java
+++ encryptdecrypt/EncryptorUtil.java
@@ -1,66 +1,85 @@
 package encryptdecrypt;
 
 public class EncryptorUtil {
+    private static final String UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWER_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
     public static String getEncryptedString(String plainString, int key, String algorithm) {
+        if ("shift".equals(algorithm)) {
+            return shiftEncryption(plainString, key);
+
+        } else {
+            return unicodeEncryption(plainString, key);
+        }
+    }
+
+    private static String unicodeEncryption(String plainString, int key) {
         StringBuilder result = new StringBuilder();
 
-        if ("shift".equals(algorithm)) {
-            final String UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            final String LOWER_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        for (char c : plainString.toCharArray()) {
+            int newUnicodeIndex = c + key;
+            result.append((char) newUnicodeIndex);
+        }
+        return result.toString();
+    }
 
-            for (char c : plainString.toCharArray()) {
-                if (Character.isAlphabetic(c)) {
-                    if (Character.isLowerCase(c)) {
-                        int newIndex = (LOWER_ALPHABET.indexOf(c) + key) % LOWER_ALPHABET.length();
-                        result.append(LOWER_ALPHABET.charAt(newIndex));
-                    } else {
-                        int newIndex = (UPPER_ALPHABET.indexOf(c) + key) % UPPER_ALPHABET.length();
-                        result.append(UPPER_ALPHABET.charAt(newIndex));
-                    }
+    private static String shiftEncryption(String plainString, int key) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : plainString.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                if (Character.isLowerCase(c)) {
+                    int newIndex = (LOWER_ALPHABET.indexOf(c) + key) % LOWER_ALPHABET.length();
+                    result.append(LOWER_ALPHABET.charAt(newIndex));
                 } else {
-                    result.append(c);
+                    int newIndex = (UPPER_ALPHABET.indexOf(c) + key) % UPPER_ALPHABET.length();
+                    result.append(UPPER_ALPHABET.charAt(newIndex));
                 }
-            }
-        } else {
-            for (char c : plainString.toCharArray()) {
-                int newUnicodeIndex = c + key;
-                result.append((char) newUnicodeIndex);
+            } else {
+                result.append(c);
             }
         }
-
         return result.toString();
     }
 
     public static String getDecryptedString(String encryptedString, int key, String algorithm) {
+        if ("shift".equals(algorithm)) {
+            return shiftDecryption(encryptedString, key);
+        } else {
+            return unicodeDecryption(encryptedString, key);
+        }
+    }
+
+    private static String unicodeDecryption(String encryptedString, int key) {
         StringBuilder result = new StringBuilder();
 
-        if ("shift".equals(algorithm)) {
-            final String UPPER_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            final String LOWER_ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+        for (char c : encryptedString.toCharArray()) {
+            int newUnicodeIndex = c - key;
+            result.append((char) newUnicodeIndex);
+        }
+        return result.toString();
+    }
 
-            for (char c : encryptedString.toCharArray()) {
-                if (Character.isAlphabetic(c)) {
-                    if (Character.isLowerCase(c)) {
-                        int newIndex = LOWER_ALPHABET.indexOf(c) - key;
-                        if (newIndex < 0) {
-                            newIndex += LOWER_ALPHABET.length();
-                        }
-                        result.append(LOWER_ALPHABET.charAt(newIndex));
-                    } else {
-                        int newIndex = UPPER_ALPHABET.indexOf(c) - key;
-                        if (newIndex < 0) {
-                            newIndex += UPPER_ALPHABET.length();
-                        }
-                        result.append(UPPER_ALPHABET.charAt(newIndex));
+    private static String shiftDecryption(String encryptedString, int key) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : encryptedString.toCharArray()) {
+            if (Character.isAlphabetic(c)) {
+                if (Character.isLowerCase(c)) {
+                    int newIndex = LOWER_ALPHABET.indexOf(c) - key;
+                    if (newIndex < 0) {
+                        newIndex += LOWER_ALPHABET.length();
                     }
+                    result.append(LOWER_ALPHABET.charAt(newIndex));
                 } else {
-                    result.append(c);
+                    int newIndex = UPPER_ALPHABET.indexOf(c) - key;
+                    if (newIndex < 0) {
+                        newIndex += UPPER_ALPHABET.length();
+                    }
+                    result.append(UPPER_ALPHABET.charAt(newIndex));
                 }
-            }
-        } else {
-            for (char c : encryptedString.toCharArray()) {
-                int newUnicodeIndex = c - key;
-                result.append((char) newUnicodeIndex);
+            } else {
+                result.append(c);
             }
         }
         return result.toString();
